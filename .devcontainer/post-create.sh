47cba49b000f4d1719319df160374c3719dfec65
Wade Barnes <wade@neoterictech.ca>
2026-04-18 12:33:56 -0700

    Add initial dev container implementation.
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+export SCRIPT_DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
+
+# echo "[post-create] Installing system dependencies"
+# apt-get update && apt-get install -y curl tar wget postgresql-client && apt-get clean
+
+echo "[post-create] Installing OpenShift CLI"
+source "${SCRIPT_DIR}/installOcCli"
+
+# echo "[post-create] Installing Source-to-Image (s2i)"
+# source "${SCRIPT_DIR}/installS2i"
+
+echo "[post-create] Installing testConnection"
+source "${SCRIPT_DIR}/installTestConnection"
+
+echo "[post-create] Restoring packages"
+source "${SCRIPT_DIR}/restoreDependencies"
