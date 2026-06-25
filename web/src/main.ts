9d7ebb725e4a09b743d97cf1009c6562f879a56d
Bryce Martel <bryce.martel@number41media.com>
2026-06-25 14:40:25 -0700

    further style cleanup

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,14 +1,13 @@
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'vuetify/styles'
 // import { createVuetify } from 'vuetify'
 import { registerPlugins } from './plugins'
 
-import "@bcgov/bc-sans/css/BC_Sans.css"
-import "@bcgov/design-tokens/css/variables.css"
-import "./styles/main.scss"
+import '@bcgov/bc-sans/css/BC_Sans.css'
+import '@bcgov/design-tokens/css/variables.css'
+import './styles/main.scss'
 
 const pinia = createPinia()
 const app = createApp(App)
